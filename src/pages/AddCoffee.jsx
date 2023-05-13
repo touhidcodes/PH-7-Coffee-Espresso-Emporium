79b136cc95e871e4c23c5668d41483bebcbd2a7b
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-14 00:59:11 +0600

    add-coffee-form

--- src/pages/AddCoffee.jsx
+++ src/pages/AddCoffee.jsx
@@ -2,24 +2,96 @@ import React from "react";
 
 const AddCoffee = () => {
 	return (
-		<div>
-			<form>
-				<div>
+		<div className='mx-auto w-2/4 mt-20'>
+			<form className='bg-slate-200 p-5 rounded-xl'>
+				<div className='flex justify-between mt-10'>
 					<div className='form-control'>
-						<label className='label'>
-							<span className='label-text'>Coffee Name</span>
-						</label>
 						<label className='input-group'>
-							<span>Name</span>
+							<span>Coffee Name</span>
 							<input
 								type='text'
 								placeholder='coffee name'
 								className='input input-bordered'
+								name='name'
+							/>
+						</label>
+					</div>
+					<div className='form-control'>
+						<label className='input-group'>
+							<span>Chef</span>
+							<input
+								type='text'
+								placeholder='chef name'
+								className='input input-bordered'
+								name='chef'
 							/>
 						</label>
 					</div>
 				</div>
-				<button className='btn btn-primary'>Button</button>
+				<div className='flex justify-between mt-10'>
+					<div className='form-control'>
+						<label className='input-group'>
+							<span>Supplier</span>
+							<input
+								type='text'
+								placeholder='supplier name'
+								className='input input-bordered'
+								name='supplier'
+							/>
+						</label>
+					</div>
+					<div className='form-control'>
+						<label className='input-group'>
+							<span>Taste</span>
+							<input
+								type='text'
+								placeholder='taste'
+								className='input input-bordered'
+								name='taste'
+							/>
+						</label>
+					</div>
+				</div>
+				<div className='flex justify-between mt-10'>
+					<div className='form-control'>
+						<label className='input-group'>
+							<span>Category</span>
+							<input
+								type='text'
+								placeholder='category'
+								className='input input-bordered'
+								name='category'
+							/>
+						</label>
+					</div>
+					<div className='form-control'>
+						<label className='input-group'>
+							<span>Details</span>
+							<input
+								type='text'
+								placeholder='details'
+								className='input input-bordered'
+								name='details'
+							/>
+						</label>
+					</div>
+				</div>
+				<div className='flex justify-between mt-10'>
+					<div className='form-control'>
+						<label className='input-group'>
+							<span>Photo URL</span>
+							<input
+								type='text'
+								placeholder='photo url'
+								className='input input-bordered'
+								name='url'
+							/>
+						</label>
+					</div>
+				</div>
+				<button className='btn btn-active btn-ghost mx-auto w-full mt-10 mb-3'>
+					Add Coffee
+				</button>
 			</form>
 		</div>
 	);
