836a284b4923665d5b4b2519eb4a4c0387ec87b0
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-14 15:35:43 +0600

    delete-data

--- src/pages/CoffeeCard.jsx
+++ src/pages/CoffeeCard.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-	const { name, chef, supplier, taste, category, details, url } = coffee;
+	const { _id, name, chef, supplier, taste, category, details, url } = coffee;
+
+	const handleDelete = (id) => {
+		console.log(id);
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				fetch(`http://localhost:5000/coffee/${id}`, {
+					method: "DELETE",
+				})
+					.then((res) => res.json())
+					.then((data) => {
+						console.log(data);
+						if (data.deletedCount > 0) {
+							Swal.fire("Deleted!", "Your file has been deleted.", "success");
+						}
+
+					});
+			}
+		});
+	};
 	return (
 		<div>
 			<div className='card card-side bg-base-100 shadow-xl'>
@@ -18,7 +46,14 @@ const CoffeeCard = ({ coffee }) => {
 						<div className='btn-group btn-group-vertical space-y-2'>
 							<button className='btn btn-active'>View</button>
 							<button className='btn'>Edit</button>
-							<button className='btn'>Delete</button>
+							<button
+								className='btn bg-orange-400 border-none'
+								onClick={() => {
+									handleDelete(_id);
+								}}
+							>
+								Delete
+							</button>
 						</div>
 					</div>
 				</div>
